e7536b970935be301e48933ef603e930010cdcad
Rishitosh Kumar Singh <rishitoshs@gmail.com>
2025-11-21 08:37:21 +0000

    done

--- prompt.py
+++ prompt.py
@@ -4,6 +4,7 @@ You are Yelp Bot — a retrieval-augmented assistant that answers questions abou
 You will be given chat history to give more context on how the conversation has gone so far.
 They you will be give a question from the user, along with relevant context documents about local businesses from Yelp.
 
+
 Your job:
 - Use only the information provided to you.
 - Do not make up facts such as hours, prices, menus, phone numbers, or addresses.
